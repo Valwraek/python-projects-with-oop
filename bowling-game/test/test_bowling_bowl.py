d19338742020ea453f00cc3684e50c1fbe9462d7
Pistacho14 <pablogonzaleztemes@gmail.com>
2025-01-17 13:35:51 +0100

    test(test): :white_check_mark: add test_miss_sign_to_num_conversor():

--- bowling-game/test/test_bowling_bowl.py
+++ bowling-game/test/test_bowling_bowl.py
@@ -9,3 +9,9 @@ def test_get_pins():
     PINS = "12345123451234512345"
     card = Score_card(PINS)
     assert PINS[0] == card.get_pins()[0]
+    
+    
+def test_miss_sign_to_num_conversor():
+    PINS = "1234-123451-3451234-"
+    card = Score_card(PINS)
+    assert list("12340123451034512340") == card.miss_sign_to_num_conversor()
