218ea89313b44f964a870cc055f2225a5d8b06bd
Pistacho14 <pablogonzaleztemes@gmail.com>
2025-01-17 13:36:45 +0100

    feat(src): :sparkles: add miss_sign_to_num_conversor

--- bowling-game/src/bowling_bowl.py
+++ bowling-game/src/bowling_bowl.py
@@ -7,4 +7,7 @@ class Score_card():
     def get_pins(self):
         return self.pins
         
-            
+    def miss_sign_to_num_conversor(self):
+        converted_pins = list(map(lambda x: "0" if x == "-" else x, self.pins))
+        return converted_pins
+        
