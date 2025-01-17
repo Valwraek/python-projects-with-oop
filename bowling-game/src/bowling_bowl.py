44a69515b4463435bac8d8baa53239012889387e
Pistacho14 <pablogonzaleztemes@gmail.com>
2025-01-17 13:04:51 +0100

    feat(src): :sparkles: add function get_pins():

--- bowling-game/src/bowling_bowl.py
+++ bowling-game/src/bowling_bowl.py
@@ -2,6 +2,9 @@ class Score_card():
     
     def __init__(self, pins):
         
-        self.pins = pins
+        self.pins = list(pins)
         
-    
+    def get_pins(self):
+        return self.pins
+        
+            
