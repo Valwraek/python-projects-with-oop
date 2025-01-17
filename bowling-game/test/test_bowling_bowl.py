aad94b76074d25f0626487fbdf8ce14bdde0d8b9
Pistacho14 <pablogonzaleztemes@gmail.com>
2025-01-17 13:03:48 +0100

    test(test): :white_check_mark: add test_get_pins():

--- bowling-game/test/test_bowling_bowl.py
+++ bowling-game/test/test_bowling_bowl.py
@@ -3,3 +3,9 @@ from src.bowling_bowl import Score_card
 def test_exist_score_card():
     card = Score_card("1234567890")
     assert card
+   
+    
+def test_get_pins():
+    PINS = "12345123451234512345"
+    card = Score_card(PINS)
+    assert PINS[0] == card.get_pins()[0]
